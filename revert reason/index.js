e96e1a4d113342c5132101b2ea670630914e4f0f
Nicholas Addison <>
2019-09-11 22:37:56 +1000

    Added validation of transaction hash argument. Get the transaction receipt to see if the transaction actually failed. Report if the transaction failed because it ran out of gas. Added Infura provider support. Improved the decoding of the reason code.

--- revert reason/index.js
+++ revert reason/index.js
@@ -9,8 +9,33 @@ const mainnetMEW = 'https://api.mycryptoapi.com/eth'
 const web3 = new Web3(mainnetTrust)
 
 async function main() {
+
+  if ( process.argv.length < 3) {
+    console.error('Missing transaction hash argument which must be a 32 byte hex string with a 0x prefix which is 64 characters in total.')
+    process.exit(1)
+  }
   const txHash = process.argv[2]
-  const { to, input, from, value, gas, gasPrice } = await web3.eth.getTransaction(txHash)
+
+  if (!txHash.match(/^0x([A-Fa-f0-9]{64})$/)) {
+    console.error('Invalid transaction hash argument. Must be a 32 byte hex string with a 0x prefix which is 64 characters in total.')
+    process.exit(2)
+  }
+
+  const receipt = await web3.eth.getTransactionReceipt(txHash)
+
+  if (receipt.status) {
+    console.error('Transaction did not fail. Can only read the revert reason from failed transactions')
+    process.exit(3)
+  }
+
+  const { to, input, from, value, gas, gasPrice, blockNumber } = await web3.eth.getTransaction(txHash)
+
+  if (receipt.gasUsed === gas) {
+    console.error('Transaction failed as it ran out of gas.')
+    process.exit(4)
+  }
+
+  let rawMessageData
   try {
     const result = await web3.eth.call({
       to,
@@ -19,14 +44,48 @@ async function main() {
       value,
       gas,
       gasPrice
-    })
-    console.log('Cannot reproduce the issue. Smart contract returns the next data', result)
+    }, blockNumber)
+
+    console.log('RAW result:', result)
+    // Trim the 0x prefix
+    rawMessageData = result.slice(2)
   } catch (e) {
-    console.log('RAW message:', e.message)
-    const rawMessageData = JSON.parse(e.message.slice(12)).data.slice(9)
-    const reason = web3.utils.hexToAscii(rawMessageData)
-    console.log('\nDecoded reason:', reason.slice(4))
+    console.log('RAW error message:', e.message)
+
+    if (e.message.startsWith('Node error: ')) {
+      // Trim "Node error: "
+      const errorObjectStr = e.message.slice(12)
+      // Parse the error object
+      const errorObject = JSON.parse(errorObjectStr)
+
+      if (!errorObject.data) {
+        throw Error('Failed to parse data field error object:' + errorObjectStr)
+      }
+
+      if (errorObject.data.startsWith('Reverted 0x')) {
+        // Trim "Reverted 0x" from the data field
+        rawMessageData = errorObject.data.slice(11)
+      }
+      else if (errorObject.data.startsWith('0x')) {
+        // Trim "0x" from the data field
+        rawMessageData = errorObject.data.slice(2)
+      }
+      else {
+        throw Error('Failed to parse data field error object:' + errorObjectStr)
+      }
+    }
+    else {
+      throw Error('Failed to parse error message from Ethereum call: ' + e.message)
+    }
   }
+
+  // Get the length of the revert reason
+  const strLen = parseInt(rawMessageData.slice(8 + 64, 8 + 128), 16)
+  // Using the length and known offset, extract and convert the revert reason
+  const reasonCodeHex = rawMessageData.slice(8 + 128, 8 + 128 + (strLen * 2))
+  // Convert reason from hex to string
+  const reason = web3.utils.hexToAscii('0x' + reasonCodeHex)
+  console.log('\nDecoded reason: "' + reason + '"')
 }
 
 main()
